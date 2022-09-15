2b1b713e0abb3979cb6ca7d92ad70155b70c33a7
Evgenii Tiurin <lawyertyurin@gmail.com>
2022-09-15 16:40:19 +0300

    overwriting a task 10

--- ATask13/Program.cs
+++ ATask13/Program.cs
@@ -4,11 +4,13 @@
 // 32679 -> 6
 
 Console.Write("Введи число: ");
-int anyNumber = Convert.ToInt32(Console.ReadLine());
+int anyNumber = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2){
+if (anyNumberText.Length > 2)
+{
   Console.WriteLine("третья цифра -> " + anyNumberText[2]);
 }
-else {
+else
+{
   Console.WriteLine("-> третьей цифры нет");
 }
